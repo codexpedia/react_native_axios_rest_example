def14bf56a19019ac57e6f002fbfe0b6cd9115cc
codexpedia <codexpedia.com@gmail.com>
2020-03-23 21:08:24 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import getGithubAccountData from './accountService';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {accountData: 'loading...'}
+  }
+
+  render() {
+    this.showData();
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Account Data</Text>
+        <Text style={styles.data}>{this.state.accountData}</Text>
+      </View>
+    );
+  }
+
+  showData() {
+    getGithubAccountData()
+    .then(data => {
+      this.setState({accountData: JSON.stringify(data)});
+    })
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  data: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
